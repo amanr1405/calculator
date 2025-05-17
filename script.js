b0059bc5a46f629f5d3bc898c14318af51dc1f26
amanr1405 <amanraj029965@gmail.com>
2025-05-17 15:11:41 +0530

    logic updated

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-var one= document.getElementsByClassName("one");
-var submit= document.getElementsByClassName("submit");
+function one(){
+    document.getElementById("result").innerText = "1";
+}
 
-function display(){
-    document.getElementById("result").innerText = "You clicked the button!";
+function two(){
+    document.getElementById("result").innerText = "2";
+}
+
+function three(){
+    document.getElementById("result").innerText = "3";
+}
+
+function four(){
+    document.getElementById("result").innerText = "4";
+}
+
+function five(){
+    document.getElementById("result").innerText = "5";
+}
+
+function six(){
+    document.getElementById("result").innerText = "6";
+}
+
+function seven(){
+    document.getElementById("result").innerText = "7";
+}
+
+function eight(){
+    document.getElementById("result").innerText = "8";
+}
+
+function nine(){
+    document.getElementById("result").innerText = "9";
+}
+
+function zero(){
+    document.getElementById("result").innerText = "0";
+}
+
+function add(){
+    document.getElementById("result").innerText = "+";
+}
+
+function sub(){
+    document.getElementById("result").innerText = "-";
+}
+
+function mul(){
+    document.getElementById("result").innerText = "*";
+}
+
+function div(){
+    document.getElementById("result").innerText = "/";
+}
+
+function del(){
+    document.getElementById("result").innerText = "";
+}
+
+function mod(){
+    document.getElementById("result").innerText = "%";
 }
