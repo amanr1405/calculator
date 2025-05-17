06cb3cda2723e5355afcba2450974776a9844dc9
amanr1405 <amanraj029965@gmail.com>
2025-05-17 13:35:43 +0530

    adding logic

---
+++ script.js
@@ -0,0 +1,6 @@
+var one= document.getElementsByClassName("one");
+var submit= document.getElementsByClassName("submit");
+
+function display(){
+    document.getElementById("result").innerText = "You clicked the button!";
+}
